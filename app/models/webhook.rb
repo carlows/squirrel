94c632752d6214c641df08b17e5d39cb261a5b04
Carlos Martinez <carlos.martinez@medwing.com>
2025-08-07 22:15:45 +0200

    Fix secret

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -5,18 +5,19 @@ class Webhook
 
   def self.verify_signature(request)
     signature = request.headers["X-Hub-Signature-256"]
-      raise WebhookSignatureError.new("No signature provided") unless signature.present?
 
-      webhook_secret = Rails.application.credentials.github[:github_webhook_secret]
+    raise WebhookSignatureError.new("No signature provided") unless signature.present?
 
-      payload_body = request.raw_post
-      signature_to_verify = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
-                                                             webhook_secret,
-                                                             payload_body)}"
+    webhook_secret = Rails.application.credentials.github_webhook_secret
 
-      unless ActiveSupport::SecurityUtils.secure_compare(signature, signature_to_verify)
-        raise WebhookSignatureError.new("Invalid signature")
-      end
+    payload_body = request.raw_post
+    signature_to_verify = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
+                                                            webhook_secret,
+                                                            payload_body)}"
+
+    unless ActiveSupport::SecurityUtils.secure_compare(signature, signature_to_verify)
+      raise WebhookSignatureError.new("Invalid signature")
+    end
   rescue StandardError => e
     raise WebhookSignatureError.new("Error verifying webhook signature: #{e.message}")
   end
